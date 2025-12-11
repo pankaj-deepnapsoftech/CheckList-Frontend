9847ff84b71c12dcf0c6370bdd557f375c5cd548
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-12-11 12:51:11 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import Login from './component/Login';
 function App() {
  
 
   return (
     <>
       <h1 class="text-3xl text-red-400 font-bold underline">Hello world!</h1>
+
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </>
   );
 }
