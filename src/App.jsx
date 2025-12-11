d7fc040e38d9622508d3e9bd563f69d955836119
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-12-11 14:23:45 +0530

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Router, Routes } from 'react-router-dom';
 import './App.css'
-import Login from './component/Login';
-import UserRole from './page/UserRole';
-import Process from './page/Process';
-import PlantName from './page/PlantName';
-import Employee from './page/Employee';
-import Company from './page/Company';
-import AssemblyLineStatus from './page/AssemblyLineStatus';
-import AssemblyLine from './page/AssemblyLine';
-import Dashboard from './page/Dashboard';
+import Login from './pages/auth/Login';
+import UserRole from './pages/UserRole';
+import Process from './pages/Process';
+import PlantName from './pages/PlantName';
+import Employee from './pages/Employee';
+import Company from './pages/Company';
+import AssemblyLineStatus from './pages/AssemblyLineStatus';
+import AssemblyLine from './pages/AssemblyLine';
+import Dashboard from './pages/Dashboard';
 function App() {
  
 
