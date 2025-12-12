9de060cae94962930c48df2dfddb4df392862f38
nitinthakurdev <radheradhe9870023@gmail.com>
2025-12-12 10:31:45 +0530

    Making sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { Route, Router, Routes } from 'react-router-dom';
-import './App.css'
-import Login from './component/Login';
-import UserRole from './page/UserRole';
-import Process from './page/Process';
-import PlantName from './page/PlantName';
-import Employee from './page/Employee';
-import Company from './page/Company';
-import AssemblyLineStatus from './page/AssemblyLineStatus';
-import AssemblyLine from './page/AssemblyLine';
-import Dashboard from './page/Dashboard';
-function App() {
- 
+import { Routes, Route } from "react-router-dom";
+import Login from "./component/Login";
+import UserRole from "./page/UserRole";
+import Process from "./page/Process";
+import PlantName from "./page/PlantName";
+import Employee from "./page/Employee";
+import Company from "./page/Company";
+import AssemblyLineStatus from "./page/AssemblyLineStatus";
+import AssemblyLine from "./page/AssemblyLine";
+import Dashboard from "./page/Dashboard";
+import MainLayout from "./layout/Mainlayout";
 
+
+function App() {
   return (
-    <> 
-        <Routes>
-          <Route path="/login" element={<Login />} />
+    <>
+      <Routes>
+
+     
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Dashboard />} />   
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/user-role" element={<UserRole />} />
           <Route path="/process" element={<Process />} />
@@ -24,10 +29,11 @@ function App() {
           <Route path="/company" element={<Company />} />
           <Route path="/assembly-line-status" element={<AssemblyLineStatus />} />
           <Route path="/assembly-line" element={<AssemblyLine />} />
-          {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
-        </Routes>
+        </Route>
+
+      </Routes>
     </>
   );
 }
 
-export default App
+export default App;
--- src/layout/Mainlayout.jsx
+++ src/layout/Mainlayout.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 import Sidebar from "../Components/Sidebar";
-import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
 
     return (
         <div className="flex h-screen bg-gray-50">
-            {/* <Sidebar />
+            <Sidebar />
             <div className="flex-1 flex flex-col w-24">
                 <Navbar />
                 <main className="flex-1 overflow-y-auto p-4 ">
                     <Outlet />
                 </main>
-            </div> */}
+            </div>
         </div>
     );
 };
