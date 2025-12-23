81bc9a1084dc0c588bea3f2d134d4c1c230b6ecd
nitinthakurdev <radheradhe9870023@gmail.com>
2025-12-23 12:34:38 +0530

    new changes

--- src/routes/Mainlayout.jsx
+++ src/routes/Mainlayout.jsx
@@ -4,20 +4,25 @@ import Sidebar from "../Components/Sidebar";
 
 export default function Layout({ children }) {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
- 
-   
+
   return (
-    <div className="flex h-screen w-full overflow-hidden">
-      <Sidebar isMobileOpen={isMobileOpen} setIsMobileOpen={setIsMobileOpen} />
-      
-      <div className="flex flex-col flex-1 h-full w-full relative">
+    <div className="flex min-h-screen w-full bg-gray-50">
+     
+      <Sidebar
+        isMobileOpen={isMobileOpen}
+        setIsMobileOpen={setIsMobileOpen}
+      />
+
+    
+      <div className="flex flex-col flex-1 min-w-0">
+    
         <Navbar
           isMobileOpen={isMobileOpen}
           onMenuClick={() => setIsMobileOpen(!isMobileOpen)}
         />
-        
-        <main className="flex-1 p-2 overflow-y-auto bg-gray-50">
-         {children}
+
+        <main className="flex-1  overflow-y-auto p-4">
+          {children}
         </main>
       </div>
     </div>
