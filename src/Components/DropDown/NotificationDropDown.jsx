291f5477ca388b63befe5f6804828b0438da14f0
nitinthakurdev <radheradhe9870023@gmail.com>
2025-12-24 14:16:46 +0530

    new chnages

--- src/Components/DropDown/NotificationDropDown.jsx
+++ src/Components/DropDown/NotificationDropDown.jsx
@@ -24,6 +24,7 @@ const NotificationDropdown = ({ innerRef, onClose }) => {
   };
 
 
+
   return (
     <div
       ref={innerRef}
@@ -81,10 +82,30 @@ const NotificationDropdown = ({ innerRef, onClose }) => {
 
 
         {getNotifications.isFetchingNextPage && (
-          <p className="text-center text-xs text-gray-500 py-2">
-            Loading more notifications...
-          </p>
+          <div className="flex justify-center items-center py-2">
+            <svg
+              className="w-5 h-5 text-gray-500 animate-spin"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
+              ></path>
+            </svg>
+          </div>
         )}
+
       </ul>
 
 
@@ -95,7 +116,7 @@ const NotificationDropdown = ({ innerRef, onClose }) => {
         </button>
 
         <button
-          onClick={() => MarkAllasRead.mutate(reciverId[0])}
+          onClick={() => MarkAllasRead.mutate(reciverId)}
           className="text-xs font-semibold text-blue-600 hover:underline"
         >
           Mark All as Read
