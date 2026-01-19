0f08fc7b1da1c9dbd2d8e34a93ba40998dc5d2ac
pankaj-deepnapsoftech <https://github.com/pankaj-deepnapsoftech>
2026-01-19 12:25:19 +0530

    services

--- src/hooks/Template Hooks/useReleaseGroup.js
+++ src/hooks/Template Hooks/useReleaseGroup.js
@@ -1,27 +1,34 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axiosHandler from "../../config/axiosconfig";
 import { toast } from "react-toastify";
 
-
 export const useReleaseGroup = () => {
-   const qc = useQueryClient();
+  const qc = useQueryClient();
 
-   const postReleaseGroup = useMutation({
-        mutationFn: async () => {
+  const postReleaseGroup = useMutation({
+    mutationFn: async () => {
+      const res = await axiosHandler.post("/release-group/create");
+      return res?.data;
+    },
+    onSuccess: (data) => {
+      toast.success(data?.message);
+      qc.invalidateQueries({ queryKey: [""] });
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
 
-            const res = await axiosHandler.post("/release-group/create")
-            return res?.data
-        },
-        onSuccess: (data) => {
-            toast.success(data?.message)
-            qc.invalidateQueries({ queryKey: [""] })
-        },
-        onError: (error) => {
-            toast.error(error?.response?.data?.message)
-        }
-    })
+  const getReleaseGroup = useQuery({
+    queryKey: ["release"],
+    queryFn: async () => {
+      const res = await axiosHandler.get("release-group/get");
+      return res?.data?.data;
+    },
+  });
 
-    return{
-        postReleaseGroup,
-    }
-}
+  return {
+    postReleaseGroup,
+    getReleaseGroup,
+  };
+};
