bd8e57dfef91c838190dca65be92f1fb9a52bbce
pankaj-deepnapsoftech <itsybizz@gmail.com>
2026-02-05 15:47:02 +0530

    Pagination

--- src/hooks/usePlcData.js
+++ src/hooks/usePlcData.js
@@ -2,13 +2,15 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import axiosHandler from "../config/axiosconfig";
 import { toast } from "react-toastify";
 
-export const usePlcData = (filters = {}, options = {}) => {
+export const usePlcData = (filters = {}, options = {},page,limit) => {
   const qc = useQueryClient();
   const { device_id, model, status, startDate, endDate, timestampStart, timestampEnd } = filters;
   const { live = true } = options; // live: false for history page (no auto-refresh)
+  console.log("page",page)
+  console.log("limit",limit)
 
   const getAllPlcData = useQuery({
-    queryKey: ["plc-data", { device_id, model, status, startDate, endDate, timestampStart, timestampEnd }],
+    queryKey: ["plc-data", { device_id, model, status, startDate, endDate, timestampStart, timestampEnd },page,limit],
     queryFn: async () => {
       const params = {};
       if (device_id) params.device_id = device_id;
@@ -18,6 +20,9 @@ export const usePlcData = (filters = {}, options = {}) => {
       if (endDate) params.endDate = endDate;
       if (timestampStart) params.timestampStart = timestampStart;
       if (timestampEnd) params.timestampEnd = timestampEnd;
+ if (page) params.page = page; 
+ if (limit) params.limit = limit;
+
 
       const res = await axiosHandler.get("/plc-data", { params });
       return res?.data?.data || [];
